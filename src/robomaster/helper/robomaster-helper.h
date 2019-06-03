24e3d82d429424a24e8583ad0b1362b2d77a8934
eiclpy <2571147314@qq.com>
2019-06-04 07:59:32 +0800

    init robomaster

---
+++ src/robomaster/helper/robomaster-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef ROBOMASTER_HELPER_H
+#define ROBOMASTER_HELPER_H
+
+#include "ns3/robomaster.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* ROBOMASTER_HELPER_H */
+
---
+++ src/robomaster/model/robomaster.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef ROBOMASTER_H
+#define ROBOMASTER_H
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* ROBOMASTER_H */
+
